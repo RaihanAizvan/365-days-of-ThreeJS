76b22c50914bac4740120085bc26685748970497
Mohammed Raihan <105449830+RaihanAizvan@users.noreply.github.com>
2026-01-02 22:43:03 +0530

    day 2: add origin and axis examples
    Define coordinates in 3D space with origin and axis points.

---
+++ days/day-002/cords-basics.js
@@ -0,0 +1,35 @@
+/**
+ * coordinates in 3d space
+ *
+ * every object exists at a coordinate
+ * a coordinate is written using x y and z
+ */
+
+/**
+ * origin
+ *
+ * the origin is the center of the world
+ * all values are zero here
+ */
+const origin = { x: 0, y: 0, z: 0 }
+
+/**
+ * x axis
+ *
+ * changing x moves left or right
+ */
+const xAxisPoint = { x: 5, y: 0, z: 0 }
+
+/**
+ * y axis
+ *
+ * changing y moves up or down
+ */
+const yAxisPoint = { x: 0, y: 3, z: 0 }
+
+/**
+ * z axis
+ *
+ * changing z moves forward or backward
+ */
+const zAxisPoint = { x: 0, y: 0, z: -4 }
