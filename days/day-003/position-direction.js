a113b1ad65e8057ceec1273f60a94118b4ba150d
Mohammed Raihan <105449830+RaihanAizvan@users.noreply.github.com>
2026-01-03 22:37:57 +0530

    day 3: add basic position example

---
+++ days/day-003/position-direction.js
@@ -0,0 +1,13 @@
+/**
+ * position
+ * as we learned yesterday
+ * position describes a location in space
+ * it answers the question where am i
+ */
+
+/**
+ * example position
+ *
+ * this point exists at a specific place
+ */
+const position = { x: 2, y: 1, z: -4 }
