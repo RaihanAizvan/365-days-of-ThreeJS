355180a2f6d0f1b9a59ed5f4df5e1056fc5d9d36
Mohammed Raihan <105449830+RaihanAizvan@users.noreply.github.com>
2026-01-04 22:14:38 +0530

    day 4: vector normalization function
    Added a normalization function to convert a vector to unit length.

--- days/day-004/normalization-basics.js
+++ days/day-004/normalization-basics.js
@@ -29,3 +29,29 @@ const length = getLength(direction)
  * this length is greater than one
  * which means the vector also includes distance
  */
+
+/**
+ * normalization
+ *
+ * normalization converts a vector into unit length
+ * unit length means length = 1
+ *
+ * direction stays the same
+ * magnitude is removed
+ */
+function normalize(v) {
+  const len = getLength(v)
+
+  return {
+    x: v.x / len,
+    y: v.y / len,
+    z: v.z / len
+  }
+}
+
+const normalizedDirection = normalize(direction)
+
+/**
+ * normalizedDirection points the same way
+ * but its length is now one
+ */
