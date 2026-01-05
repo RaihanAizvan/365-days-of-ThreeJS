09fd62923ebde47aef45202cf2b852f2db8da94b
Mohammed Raihan <105449830+RaihanAizvan@users.noreply.github.com>
2026-01-05 23:49:23 +0530

    day5:  local and parent object space
    Added documentation for local space and parent-child positioning.

--- days/day-005/space-basics.js
+++ days/day-005/space-basics.js
@@ -17,3 +17,22 @@ const worldPositionB = { x: -2, y: 4, z: 1 }
  * these positions are absolute
  * they do not depend on other objects
  */
+
+/**
+ * local space
+ *
+ * local space is relative to a parent object
+ * positions are measured from the parent
+ */
+
+/**
+ * parent object position in world space
+ */
+const parentPosition = { x: 10, y: 0, z: 0 }
+
+/**
+ * child position in local space
+ *
+ * this position is relative to the parent
+ */
+const childLocalPosition = { x: 2, y: 1, z: 0 }
