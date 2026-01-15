c6b98a8a1e90e56059a77dc29469ab6b07e0a7aa
Mohammed Raihan <aizvanr@gmail.com>
2026-01-15 22:53:21 +0530

    day 15: camera tips

---
+++ days/day-015/threejs-camera-tips.js
@@ -0,0 +1,33 @@
+/**
+ * camera tips (three js)
+ */
+
+/**
+ * lookAt
+ *
+ * camera.lookAt(x, y, z) rotates the camera so it points at a target.
+ *
+ * note: lookAt affects rotation, not position.
+ */
+
+/**
+ * common starting values
+ *
+ * - camera position: (0, 2, 5)
+ * - near: 0.1
+ * - far: 100
+ * - fov: 50-75
+ */
+
+/**
+ * debug helpers
+ *
+ * - AxesHelper / GridHelper for orientation
+ * - CameraHelper(camera) to visualize frustum
+ */
+
+/**
+ * remember: the camera is just another Object3D
+ *
+ * you can parent it to a group, animate it, or attach it to a player.
+ */
