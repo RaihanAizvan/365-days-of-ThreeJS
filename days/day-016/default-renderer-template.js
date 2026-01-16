2f4700ab61449974e400d6f7b10275eded2f1d20
Mohammed Raihan <aizvanr@gmail.com>
2026-01-16 23:48:46 +0530

    day 16: default renderer template

---
+++ days/day-016/default-renderer-template.js
@@ -0,0 +1,22 @@
+/**
+ * a reasonable renderer template for most projects
+ */
+
+// const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+// renderer.setSize(sizes.width, sizes.height)
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer.setClearColor(0x0f0f12, 1)
+// renderer.outputColorSpace = THREE.SRGBColorSpace
+// renderer.toneMapping = THREE.ACESFilmicToneMapping
+// renderer.toneMappingExposure = 1.0
+//
+// window.addEventListener('resize', () => {
+//   sizes.width = window.innerWidth
+//   sizes.height = window.innerHeight
+//
+//   camera.aspect = sizes.width / sizes.height
+//   camera.updateProjectionMatrix()
+//
+//   renderer.setSize(sizes.width, sizes.height)
+//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// })
