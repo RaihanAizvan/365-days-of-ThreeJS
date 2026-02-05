7f0b2ae9020fca081424dfc50e430f20b33be643
Mohammed Raihan <aizvanr@gmail.com>
2026-02-05 20:34:54 +0530

    day 35: plane geometry notes

---
+++ days/day-035/plane-geometry-basics.js
@@ -0,0 +1,25 @@
+/**
+ * day 35 - plane geometries
+ */
+
+/**
+ * PlaneGeometry(width, height, widthSegments, heightSegments)
+ * - faces +Z by default
+ */
+
+/**
+ * CircleGeometry(radius, segments)
+ * - a disc in the XY plane (faces +Z)
+ */
+
+/**
+ * RingGeometry(innerRadius, outerRadius, thetaSegments)
+ * - a donut-shaped disc
+ */
+
+/**
+ * rotation for ground
+ *
+ * to make a ground plane, rotate around X:
+ *   plane.rotation.x = -Math.PI / 2
+ */
---
+++ days/day-035/plane-usage-tips.js
@@ -0,0 +1,18 @@
+/**
+ * plane usage tips
+ */
+
+/**
+ * segments
+ *
+ * more segments => more triangles
+ * needed when you deform a plane (terrain, waves).
+ */
+
+/**
+ * double-sided
+ *
+ * planes are one-sided by default (front side).
+ * if you need both sides:
+ *   material.side = THREE.DoubleSide
+ */
