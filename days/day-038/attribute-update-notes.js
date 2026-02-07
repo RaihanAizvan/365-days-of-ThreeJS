08a608443b7050ccde5d0d423d37980911d5b58d
Mohammed Raihan <aizvanr@gmail.com>
2026-02-07 22:23:32 +0530

    day 38: geometry attributes notes

---
+++ days/day-038/attribute-update-notes.js
@@ -0,0 +1,19 @@
+/**
+ * attribute update notes
+ */
+
+/**
+ * if you update positions:
+ * - set needsUpdate
+ * - recompute normals if lighting looks wrong
+ */
+
+/**
+ * for frequent updates:
+ * - attribute.setUsage(THREE.DynamicDrawUsage)
+ */
+
+/**
+ * important:
+ * attributes are typed arrays, so update values in-place.
+ */
---
+++ days/day-038/geometry-attributes-basics.js
@@ -0,0 +1,36 @@
+/**
+ * day 38 - geometry attributes
+ */
+
+/**
+ * BufferGeometry attributes
+ *
+ * geometry.attributes is a map of BufferAttributes.
+ * common keys:
+ * - position
+ * - normal
+ * - uv
+ * - color
+ */
+
+/**
+ * attribute count
+ *
+ * attribute.count = number of vertices
+ *
+ * for position:
+ * count = positions.length / 3
+ */
+
+/**
+ * updating attributes
+ *
+ * if you modify attribute.array, set:
+ * - attribute.needsUpdate = true
+ */
+
+/**
+ * debugging
+ *
+ * console.log(geometry.attributes)
+ */
