861ffeddd356ddcf291fa22652f2d37a7fdd5928
Mohammed Raihan <aizvanr@gmail.com>
2026-02-14 15:33:18 +0530

    day 45: texture loading notes

---
+++ days/day-045/loading-manager-tips.js
@@ -0,0 +1,18 @@
+/**
+ * loading manager tips
+ */
+
+/**
+ * show a progress bar
+ * - update width: loaded / total
+ */
+
+/**
+ * handle errors gracefully
+ * - show a fallback texture
+ */
+
+/**
+ * avoid blocking render loop
+ * - keep rendering while loading
+ */
---
+++ days/day-045/texture-loading-basics.js
@@ -0,0 +1,27 @@
+/**
+ * day 45 - texture loading
+ */
+
+/**
+ * TextureLoader
+ */
+
+// const loader = new THREE.TextureLoader()
+// loader.load(
+//   '/textures/wood.jpg',
+//   (texture) => { /* onLoad */ },
+//   (event) => { /* onProgress */ },
+//   (error) => { /* onError */ }
+// )
+
+/**
+ * LoadingManager
+ */
+
+// const manager = new THREE.LoadingManager()
+// manager.onStart = () => {}
+// manager.onLoad = () => {}
+// manager.onProgress = (url, loaded, total) => {}
+// manager.onError = (url) => {}
+//
+// const loader = new THREE.TextureLoader(manager)
