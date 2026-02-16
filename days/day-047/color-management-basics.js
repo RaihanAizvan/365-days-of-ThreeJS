bd60d0450a2b1a52d88f5197d70d70e9d7d363d5
Mohammed Raihan <aizvanr@gmail.com>
2026-02-16 23:20:44 +0530

    day 47: color management notes

---
+++ days/day-047/color-management-basics.js
@@ -0,0 +1,21 @@
+/**
+ * day 47 - color management
+ */
+
+/**
+ * sRGB vs linear
+ *
+ * - textures that represent color should be in sRGB
+ * - data textures should remain linear
+ */
+
+/**
+ * three js defaults
+ *
+ * renderer.outputColorSpace = THREE.SRGBColorSpace
+ * texture.colorSpace = THREE.SRGBColorSpace (for color textures)
+ */
+
+/**
+ * if colors look too dark/light, check color space first.
+ */
---
+++ days/day-047/color-space-tips.js
@@ -0,0 +1,20 @@
+/**
+ * color space tips
+ */
+
+/**
+ * color textures
+ * - albedo/baseColor, emissive
+ */
+
+/**
+ * data textures
+ * - normal, roughness, metalness, ao
+ * should remain linear.
+ */
+
+/**
+ * tone mapping
+ *
+ * tone mapping happens after color space conversion.
+ */
