dfd967ebdeeaa7eb1709ee18c593c6dbb674ed77
Mohammed Raihan <aizvanr@gmail.com>
2026-02-18 23:58:49 +0530

    day 49: filtering modes notes

---
+++ days/day-049/filtering-modes-basics.js
@@ -0,0 +1,28 @@
+/**
+ * day 49 - filtering modes
+ */
+
+/**
+ * minification filters
+ *
+ * used when texture is smaller than a pixel on screen.
+ * common:
+ * - LinearMipMapLinearFilter (default)
+ * - LinearFilter (no mipmaps)
+ * - NearestFilter (no mipmaps)
+ */
+
+/**
+ * magnification filters
+ *
+ * used when texture is larger than a pixel on screen.
+ * only two options:
+ * - LinearFilter
+ * - NearestFilter
+ */
+
+/**
+ * pixel art
+ *
+ * use NearestFilter for crisp pixels.
+ */
---
+++ days/day-049/filtering-tips.js
@@ -0,0 +1,19 @@
+/**
+ * filtering tips
+ */
+
+/**
+ * mipmaps
+ *
+ * minFilter options that include MipMap require power-of-two textures.
+ */
+
+/**
+ * if you set magFilter to Nearest, the texture will look pixelated when upscaled.
+ */
+
+/**
+ * note
+ *
+ * magnification uses only nearest/linear; no mipmaps.
+ */
