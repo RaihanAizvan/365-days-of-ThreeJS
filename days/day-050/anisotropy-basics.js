94614b79a0405ec47ab2088433b3e17a6bdbcfea
Mohammed Raihan <aizvanr@gmail.com>
2026-02-19 23:23:07 +0530

    day 50: anisotropy notes

---
+++ days/day-050/anisotropy-basics.js
@@ -0,0 +1,23 @@
+/**
+ * day 50 - anisotropy theory
+ */
+
+/**
+ * anisotropy
+ *
+ * increases texture sampling quality when viewing at steep angles.
+ * more samples along the direction of distortion.
+ */
+
+/**
+ * usage
+ *
+ * const max = renderer.capabilities.getMaxAnisotropy()
+ * texture.anisotropy = Math.min(8, max)
+ */
+
+/**
+ * cost
+ *
+ * higher anisotropy = better quality but more GPU cost.
+ */
---
+++ days/day-050/anisotropy-tips.js
@@ -0,0 +1,16 @@
+/**
+ * anisotropy tips
+ */
+
+/**
+ * ideal targets
+ *
+ * - floors, roads, walls seen at shallow angles
+ */
+
+/**
+ * typical values
+ *
+ * - 2, 4, 8 are common
+ * - use renderer.capabilities.getMaxAnisotropy() as upper bound
+ */
