360ef3bb10e825da66c0642e1f8b8ed0dc145472
Mohammed Raihan <aizvanr@gmail.com>
2026-02-20 21:35:50 +0530

    day 51: repeat wrapping notes

---
+++ days/day-051/repeat-wrapping-basics.js
@@ -0,0 +1,23 @@
+/**
+ * day 51 - repeat wrapping
+ */
+
+/**
+ * wrap modes
+ *
+ * texture.wrapS = THREE.RepeatWrapping
+ * texture.wrapT = THREE.RepeatWrapping
+ */
+
+/**
+ * tiling
+ *
+ * texture.repeat.set(u, v)
+ */
+
+/**
+ * offset
+ *
+ * texture.offset.set(u, v)
+ * can be animated to scroll textures.
+ */
---
+++ days/day-051/repeat-wrapping-tips.js
@@ -0,0 +1,16 @@
+/**
+ * repeat wrapping tips
+ */
+
+/**
+ * to repeat, texture must be power-of-two if you want mipmaps.
+ */
+
+/**
+ * clamp-to-edge is the default wrap mode.
+ */
+
+/**
+ * mirror wrapping is another option:
+ *   texture.wrapS = THREE.MirroredRepeatWrapping
+ */
