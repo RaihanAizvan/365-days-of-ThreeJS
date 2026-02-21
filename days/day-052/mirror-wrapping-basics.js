c88c6982954467f20d3fca370f2046cc8beee65e
Mohammed Raihan <aizvanr@gmail.com>
2026-02-21 15:12:48 +0530

    day 52: mirror wrapping notes

---
+++ days/day-052/mirror-wrapping-basics.js
@@ -0,0 +1,22 @@
+/**
+ * day 52 - mirror wrapping
+ */
+
+/**
+ * mirror wrapping
+ *
+ * texture.wrapS = THREE.MirroredRepeatWrapping
+ * texture.wrapT = THREE.MirroredRepeatWrapping
+ */
+
+/**
+ * how it works
+ *
+ * each repeated tile is flipped every other repeat.
+ */
+
+/**
+ * combine with repeat
+ *
+ * texture.repeat.set(2, 2)
+ */
---
+++ days/day-052/mirror-wrapping-tips.js
@@ -0,0 +1,11 @@
+/**
+ * mirror wrapping tips
+ */
+
+/**
+ * mirror wrap reduces obvious seams in some textures.
+ */
+
+/**
+ * use it for patterns that should alternate naturally.
+ */
