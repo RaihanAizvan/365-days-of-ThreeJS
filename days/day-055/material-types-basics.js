2ba49a26d424ea0a11ce1f2dd7ff8a50d42bacec
Mohammed Raihan <aizvanr@gmail.com>
2026-02-24 14:57:57 +0530

    day 55: add material types overview

---
+++ days/day-055/material-types-basics.js
@@ -0,0 +1,44 @@
+/**
+ * day 55 - material types
+ */
+
+/**
+ * MeshBasicMaterial
+ *
+ * unlit material, no lighting calculations.
+ * shows solid color or texture.
+ * fast but unrealistic.
+ */
+
+/**
+ * MeshStandardMaterial
+ *
+ * physically-based rendering (PBR).
+ * realistic lighting and reflections.
+ * industry standard for most 3D.
+ */
+
+/**
+ * MeshPhongMaterial
+ *
+ * legacy specular lighting model.
+ * cheaper than Standard.
+ * less realistic highlights.
+ */
+
+/**
+ * MeshLambertMaterial
+ *
+ * simplest lighting model.
+ * diffuse only, no specular.
+ * cheap but flat-looking.
+ */
+
+/**
+ * other material types
+ *
+ * MeshMatcapMaterial: baked lighting
+ * MeshToonMaterial: cel-shading
+ * MeshNormalMaterial: debug normals
+ * MeshDepthMaterial: depth visualization
+ */
